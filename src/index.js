d44e2dcb5ed4c4a92963fabbb38b8f127e994485
wangming <1603657728@qq.com>
2020-05-04 19:31:04 +0800

    3 打包图片资源和html文件里的图片

--- src/index.js
+++ src/index.js
@@ -1,5 +1,1 @@
-function add(a, b) {
-    return a + b
-}
-
-console.log(add(2, 3))
+import './style.scss'
--- webpack.config.js
+++ webpack.config.js
@@ -36,6 +36,34 @@ const config = {
                     // 将sass文件编译成css文件
                     'sass-loader'
                 ]
+            },
+            {
+                // 问题：默认不能处理html中img图片
+                // 处理图片资源
+                test: /\.(jpg|png|gif|jpeg)$/,
+                // 需要下载 url-loader file-loader
+                loader: 'url-loader',
+                options: {
+                    // 图片大小小于8kb，就会被转换成base64编码
+                    // 优点：减少http请求（减轻服务器压力）
+                    // 缺点：图片体积会更大（文件请求速度会更慢）
+                    limit: 20 * 1024,
+                    // 问题：因为url-loader默认使用es6模块化解析，而html-loader引入图片是commonjs
+                    // 解析时会出问题：图片路径为[object Module]
+                    // 解决：关闭 url-loader的es6模块化，使用commonjs解析
+                    // tips：在我的机器上未遇到这种问题，猜测应该是url-loader的版本问题
+                    esModule: false,
+                    // 给图片进行重命名
+                    // [name]取原文件名
+                    // [hash:12]取图片的hash前12位
+                    // [ext]取文件的原扩展名
+                    name: '[name]-[hash:12].[ext]'
+                }
+            },
+            {
+                test: /\.html$/,
+                // 处理html文件里的img图片（负责引入img，从而能被url-loader进行处理
+                loader: 'html-loader'
             }
         ]
     },
