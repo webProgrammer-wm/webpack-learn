47616f7373321f1c9d0471f98a1431e0673778b5
allen <1603657728@qq.com>
2020-05-06 10:43:00 +0800

    8 CSS添加产商前缀

--- webpack.config.js
+++ webpack.config.js
@@ -2,6 +2,8 @@ const { resolve } = require('path')
 const HtmlWebpackPlugin = require('html-webpack-plugin')
 const MiniCssExtractPlugin = require('mini-css-extract-plugin')
 
+// process.env.NODE_ENV = 'development'
+
 const config = {
     mode: 'development',
     entry: './src/js/index',
@@ -21,7 +23,37 @@ const config = {
                             publicPath: '../../'
                         }
                     },
-                    'css-loader'
+                    'css-loader',
+                    /*
+                        css 兼容性处理：postcss -> postcss-loader postcss-preset-env
+                        帮 postcss 找到 package.json 中 browserslist里面的配置，通过配置加载指定的css兼容性样式
+
+                        "browserslist": {
+                            // 开发环境 --> 设置 node 环境变量：process.env.NODE_ENV = 'development'
+                            "development": [
+                                "last 1 chrome version",
+                                "last 1 firefox version",
+                                "laST 1 safari version"
+                            ],
+                            "production": [
+                                ">0.2%",
+                                "not dead",
+                                "not op_mini all"
+                            ]
+                        }
+                     */
+                    // 使用loader的默认配置
+                    // 'postcss-loader',
+                    {
+                        loader: 'postcss-loader',
+                        options: {
+                            ident: 'postcss',
+                            plugins: () => [
+                                // postcss的插件
+                                require('postcss-preset-env')()
+                            ]
+                        }
+                    }
                 ]
             },
             {
@@ -34,7 +66,17 @@ const config = {
                         }
                     },
                     'css-loader',
-                    'sass-loader'
+                    'sass-loader',
+                    {
+                        loader: 'postcss-loader',
+                        options: {
+                            ident: 'postcss',
+                            plugins: () => [
+                                // postcss的插件
+                                require('postcss-preset-env')()
+                            ]
+                        }
+                    }
                 ]
             },
             {
@@ -66,8 +108,7 @@ const config = {
             template: './src/index.html'
         }),
         new MiniCssExtractPlugin({
-            filename: 'assets/css/[name].css',
-            publicPath: '../'
+            filename: 'assets/css/[name].[hash].css',
         })
     ],
     devServer: {
