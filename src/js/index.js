087a6eea9eaf3b225bf0f7897413b86e794665df
allen <1603657728@qq.com>
2020-05-07 10:51:03 +0800

    25 entry的详细配置

--- src/js/index.js
+++ src/js/index.js
@@ -1,3 +1,8 @@
-import $ from 'jquery'
+import add from "./add"
+import mul from "./mul"
 
-console.log($)
+console.log('index---------')
+
+
+console.log(add(1, 2))
+console.log(mul(4, 5))
--- webpack.config.js
+++ webpack.config.js
@@ -1,29 +1,42 @@
 const { resolve } = require('path')
 const HtmlWebpackPlugin = require('html-webpack-plugin')
-const webpack = require('webpack')
-const AddAssetHtmlWebpackPlugin = require('add-asset-html-webpack-plugin')
 
-// process.env.NODE_ENV = 'development'
+/*
+    entry：入口起点
+        1.string -> './src/index.js'
+            单入口
+            打包形成一个chunk，输出一个bundle文件
+            chunk名称默认是 main
+        2.array -> ['./src/index.js', './src/add.js']
+            多入口
+            所有入口文件最终只会形成一个chunk，输出出去只有一个文件
+                -> 只有在HMR功能中让html热更新生效
+        3.object
+            {
+                 index: './src/index.js',
+                 add: './src/add.js'
+            }
+            多入口
+            有几个入口文件就形成几个chunk，输出几个bundle文件
+            此时的chunk名称是key
+                -> 特殊用法
+                    {
+                         多个入口文件最终只会形成一个chunk，输出出去只有一个bundle文件
+                         index: ['./src/index.js', './src/mul.js'],
+                         形成一个chunk，输出一个bundle文件
+                         add: './src/add.js'
+                    }
+ */
 
 const config = {
-    mode: 'production',
-    entry: './src/js/index',
+    mode: 'development',
+    entry: './src/js/index.js',
     output: {
-        filename: "js/index.js",
+        filename: "js/[name].js",
         path: resolve(__dirname, 'dist')
     },
     plugins: [
-        new HtmlWebpackPlugin({
-            template: './src/index.html'
-        }),
-        // 告诉webpack哪些库不参与打包，同时使用时的名称也得变
-        new webpack.DllReferencePlugin({
-            manifest: resolve(__dirname, 'dll/manifest.json')
-        }),
-        // 将某个文件打包输出出去，并在html中自动引入资源
-        new AddAssetHtmlWebpackPlugin({
-            filepath: resolve(__dirname, 'dll/jquery.js')
-        })
+        new HtmlWebpackPlugin()
     ]
 }
 
