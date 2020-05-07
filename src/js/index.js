11ab3c349a6faf769b38aa1f94c892a589f87d86
allen <1603657728@qq.com>
2020-05-07 09:27:09 +0800

    23 externals-拒绝打包某些包

--- src/js/index.js
+++ src/js/index.js
@@ -1,28 +1,3 @@
-console.log('index文件被加载了')
+import $ from 'jquery'
 
-import { mul } from './test'
-
-document.querySelector('#btn').onclick = function () {
-    // 懒加载：当文件需要使用时才加载
-    // 预加载：webpackPrefetch: true 会提前加载js文件
-    // 正在加载可以认为是并行加载（同一时间加载多个文件）
-    // 预加载则是等其他资源加载完毕，浏览器空闲了，再偷偷加载资源
-    import(/* webpackChunkName: 'test', webpackPrefetch: true */'./test').then(({ mul }) => {
-        console.log(mul(4, 5))
-    })
-}
-
-// 注册 serviceworker
-// 处理兼容性问题
-if ('serviceWorker' in navigator) {
-    window.addEventListener('load', () => {
-        navigator.serviceWorker
-            .register('/service-worker.js')
-            .then(() => {
-                console.log('sw注册成功了')
-            })
-            .catch(() => {
-                console.log('sw注册失败了')
-            })
-    })
-}
+console.log($)
--- webpack.config.js
+++ webpack.config.js
@@ -6,114 +6,21 @@ const OptimizeCssAssetsWebpackPlugin = require('optimize-css-assets-webpack-plug
 // process.env.NODE_ENV = 'development'
 
 const config = {
-    mode: 'development',
+    mode: 'production',
     entry: './src/js/index',
     output: {
         filename: "js/index.js",
         path: resolve(__dirname, 'dist')
     },
-    module: {
-        rules: [
-            {
-                test: /\.css$/,
-                use: [
-                    {
-                        loader: MiniCssExtractPlugin.loader,
-                        options: {
-                            publicPath: '../../'
-                        }
-                    },
-                    'css-loader',
-                    {
-                        loader: 'postcss-loader',
-                        options: {
-                            ident: 'postcss',
-                            plugins: () => [
-                                // postcss的插件
-                                require('postcss-preset-env')()
-                            ]
-                        }
-                    }
-                ]
-            },
-            {
-                test: /\.(sass|scss)$/,
-                use: [
-                    {
-                        loader: MiniCssExtractPlugin.loader,
-                        options: {
-                            publicPath: '../../'
-                        }
-                    },
-                    'css-loader',
-                    'sass-loader',
-                    {
-                        loader: 'postcss-loader',
-                        options: {
-                            ident: 'postcss',
-                            plugins: () => [
-                                // postcss的插件
-                                require('postcss-preset-env')()
-                            ]
-                        }
-                    }
-                ]
-            },
-            {
-                test: /\.(jpg|png|gif|jpeg|)$/,
-                loader: 'url-loader',
-                options: {
-                    name: '[name]-[hash:8].[ext]',
-                    limit: 8 * 1024,
-                    esModule: false,
-                    outputPath: 'assets/images'
-                }
-            },
-            {
-                test: /\.html$/,
-                loader: 'html-loader'
-            },
-            {
-                test: /\.(ttf|eot|svg|woff|woff2)$/,
-                loader: 'file-loader',
-                options: {
-                    name: '[name]-[hash:8].[ext]',
-                    outputPath: 'assets/font'
-                }
-            },
-            {
-                test: /\.js$/,
-                exclude: /node_modules/,
-                use: [
-                    /* 开启多进程打包
-                       进程启动大概为600ms，进程通信也有开销
-                       只有工作消耗时间比较长，才需要多进程打包
-                     */
-                    {
-                        loader: 'thread-loader',
-                        options: {
-                            workers: 2 // 2个进程
-                        }
-                    },
-                    {
-                        loader: 'babel-loader'
-                    }
-                ]
-            }
-        ]
-    },
     plugins: [
         new HtmlWebpackPlugin({
             template: './src/index.html'
         }),
-        new MiniCssExtractPlugin({
-            filename: 'assets/css/[name].[contenthash].css',
-        }),
         new OptimizeCssAssetsWebpackPlugin()
     ],
-    devServer: {
-        compress: true,
-        port: 8000
+    externals: {
+        // 拒绝jQuery被打包进来
+        jquery: 'jQuery'
     }
 }
 
