eb7c80a1836444df54191bbe3e6cb78513252aed
allen <1603657728@qq.com>
2020-05-07 11:03:03 +0800

    26 output的详细配置

--- webpack.config.js
+++ webpack.config.js
@@ -2,38 +2,25 @@ const { resolve } = require('path')
 const HtmlWebpackPlugin = require('html-webpack-plugin')
 
 /*
-    entry：入口起点
-        1.string -> './src/index.js'
-            单入口
-            打包形成一个chunk，输出一个bundle文件
-            chunk名称默认是 main
-        2.array -> ['./src/index.js', './src/add.js']
-            多入口
-            所有入口文件最终只会形成一个chunk，输出出去只有一个文件
-                -> 只有在HMR功能中让html热更新生效
-        3.object
-            {
-                 index: './src/index.js',
-                 add: './src/add.js'
-            }
-            多入口
-            有几个入口文件就形成几个chunk，输出几个bundle文件
-            此时的chunk名称是key
-                -> 特殊用法
-                    {
-                         多个入口文件最终只会形成一个chunk，输出出去只有一个bundle文件
-                         index: ['./src/index.js', './src/mul.js'],
-                         形成一个chunk，输出一个bundle文件
-                         add: './src/add.js'
-                    }
+
  */
 
 const config = {
     mode: 'development',
     entry: './src/js/index.js',
     output: {
+        // 文件名称（指定名称 + 目录）
         filename: "js/[name].js",
-        path: resolve(__dirname, 'dist')
+        // 输出文件目录（将来所有资源输出的公共目录）
+        path: resolve(__dirname, 'dist'),
+        // 所有输出资源引入的公共路径的前缀 -> 路径的前面
+        publicPath: "/",
+        // 非入口chunk的名称
+        chunkFilename: '[name]_chunk.js',
+        // library: '[name]', // 整个库向外暴露的变量名
+        // libraryTarget: 'window', // 变量名添加到window上，browser端
+        // libraryTarget: 'global' // 变量名添加到global上，node端
+        // libraryTarget: 'commonjs'
     },
     plugins: [
         new HtmlWebpackPlugin()
